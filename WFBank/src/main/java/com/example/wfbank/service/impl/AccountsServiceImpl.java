a5d1159f11f1a328c9d2810ffc7c44cd1be28f5d
Aquib-Nawaz <aquibnawaz78692@gmail.com>
2023-08-11 12:55:07 +0530

    AccountsService

--- WFBank/src/main/java/com/example/wfbank/service/impl/AccountsServiceImpl.java
+++ WFBank/src/main/java/com/example/wfbank/service/impl/AccountsServiceImpl.java
@@ -1,5 +1,50 @@
 package com.example.wfbank.service.impl;
 
-public class AccountsServiceImpl {
+import java.util.List;
+
+import com.example.wfbank.exception.ResourceNotFoundException;
+import com.example.wfbank.model.Accounts;
+import com.example.wfbank.repository.AccountsRepository;
+import com.example.wfbank.service.AccountsService;
+
+public class AccountsServiceImpl implements AccountsService {
+
+	private AccountsRepository accountsRepository;
+	
+	public AccountsServiceImpl(AccountsRepository accountsRepository) {
+		this.accountsRepository = accountsRepository;
+	}
+	@Override
+	public Accounts saveAccounts(Accounts accounts) {
+		// TODO Auto-generated method stub
+		return accountsRepository.save(accounts);
+	}
+
+	@Override
+	public List<Accounts> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return accountsRepository.findAll();
+	}
+
+	@Override
+	public Accounts getAccountsById(long id) {
+		// TODO Auto-generated method stub
+		return accountsRepository.findById(id).orElseThrow(
+				()-> new ResourceNotFoundException("Accounts", "Id", id));
+	}
+
+	@Override
+	public Accounts updateAccounts(Accounts Accounts, long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAccounts(long id) {
+		// TODO Auto-generated method stub
+		accountsRepository.findById(id).orElseThrow(
+				()-> new ResourceNotFoundException("Accounts", "Id", id));
+		accountsRepository.deleteById(id);
+	}
 
 }
